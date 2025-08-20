06e52c5074f16eacde3c159d06a31bcc907d0f76
DroidBait66 <Alpbberrak@gmail.com>
2025-08-20 10:09:59 -0400

    char set space

--- StrenghtCheck.py
+++ StrenghtCheck.py
@@ -9,5 +9,17 @@ def passwordStrength(password: str):
   hasLower = bool(re.serach(r"[a-z]",password))
   hasUpper = bool(re.search(r"[A-Z]",password))
   hasNum = bool(re.search(r"/d", password))
-  has_symbol = bool(re.search(r"[!@#$%^&*(),.?:{}|<>]",password))
+  hasSymbol = bool(re.search(r"[!@#$%^&*(),.?:{}|<>`~-+_/]",password))
+  
+  # calculate character space from sets
+  charSpace = 0
+  
+  if hasLower:
+    charSpace += 26
+  if hasUpper:
+    charSpace += 26
+  if hasNum:
+    charSpace += 10
+  if hasSymbol:
+    charSpace += 25
   
